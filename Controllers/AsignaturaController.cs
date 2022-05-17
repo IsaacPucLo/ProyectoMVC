ba902a4f8d98f54e4f3f692b9697781ee1326da6
Isaac Puc <dual.isaac.puc@gmail.com>
2022-05-17 17:05:02 -0500

    Vistas parciales

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using ProyectoMVC.Models;
 
@@ -6,19 +7,50 @@ namespace ProyectoMVC.Controllers
 {
     public class AsignaturaController : Controller
     {
-        public IActionResult Index(){
+        
+        public IActionResult Index()
+        {
 
-            var asignatura = new Asignatura{
+            var asignatura = new Asignatura
+            {
                 UniqueId = Guid.NewGuid().ToString(),
                 Nombre = "Programación"
-
             };
-            
+
+            ViewBag.Fecha = DateTime.Now;
+
+            return View(asignatura);  
+        }
+        public IActionResult MultiAsignatura()
+        {
+
+            var listaAsignaturas = new List<Asignatura>() {
+                new Asignatura {
+                Nombre = "Matemáticas",
+                UniqueId = Guid.NewGuid ().ToString ()
+                },
+                new Asignatura {
+                Nombre = "Educación Física",
+                UniqueId = Guid.NewGuid ().ToString ()
+                },
+                new Asignatura {
+                Nombre = "Castellano",
+                UniqueId = Guid.NewGuid ().ToString ()
+                },
+                new Asignatura {
+                Nombre = "Ciencias Naturales",
+                UniqueId = Guid.NewGuid ().ToString ()
+                },
+                new Asignatura {
+                Nombre = "Programacion",
+                UniqueId = Guid.NewGuid ().ToString ()
+                }
+            };
 
             ViewBag.CosaDinamica = "La monja";
             ViewBag.Fecha = DateTime.Now;
-            
-            return View(asignatura);  //Se manda el objeto escuela como parámetro a la vista, para poder hacer uso de la información de escuela en la vista, en este caso imprimir la información
+
+            return View("MultiAsignatura",listaAsignaturas);  //Se especifica que va a lanzar la vista con el nombre entre comillas
         }
     }
 }
