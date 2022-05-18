492ab91bdd9fd020cfffd3a824bfee259a9b2da0
Isaac Puc <dual.isaac.puc@gmail.com>
2022-05-18 15:12:14 -0500

    Entity Framework y conectando la soluciòn a EF

--- Controllers/EscuelaController.cs
+++ Controllers/EscuelaController.cs
@@ -1,4 +1,15 @@
+
+ // var escuela = new Escuela();
+ // escuela.AñoDeCreación = 2005;
+ // escuela.UniqueId = Guid.NewGuid().ToString();
+ // escuela.Nombre = "TecNM Campus Progreso";
+ // escuela.Ciudad = "Progreso";
+ // escuela.Pais = "México";
+ // escuela.TipoEscuela = TiposEscuela.Secundaria;
+ // escuela.Dirección = "Boulevard 7ma";
+
 using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using ProyectoMVC.Models;
 
@@ -6,20 +17,19 @@ namespace ProyectoMVC.Controllers
 {
     public class EscuelaController : Controller
     {
-        public IActionResult Index(){
-
-            var escuela = new Escuela();
-            escuela.AñoDeCreación = 2005;
-            escuela.UniqueId = Guid.NewGuid().ToString();
-            escuela.Nombre = "TecNM Campus Progreso";
-            escuela.Ciudad = "Progreso";
-            escuela.Pais = "México";
-            escuela.TipoEscuela = TiposEscuela.Secundaria;
-            escuela.Dirección = "Boulevard 7ma";
-
+        private EscuelaContext _context;
+        public IActionResult Index()
+        {
             ViewBag.CosaDinamica = "La monja";
-            
+
+            var escuela = _context.Escuelas.FirstOrDefault();
+
             return View(escuela);  //Se manda el objeto escuela como parámetro a la vista, para poder hacer uso de la información de escuela en la vista, en este caso imprimir la información
+        }
+
+        public EscuelaController(EscuelaContext context)
+        {
+            _context = context;
         }
     }
 }
