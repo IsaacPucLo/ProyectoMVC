581e21ab2b3e9740d7dab784027738d574084139
Isaac Puc <dual.isaac.puc@gmail.com>
2022-05-19 11:33:44 -0500

    Ajuste de enrrutamiento

--- Controllers/AsignaturaController.cs
+++ Controllers/AsignaturaController.cs
@@ -8,11 +8,25 @@ namespace ProyectoMVC.Controllers
 {
     public class AsignaturaController : Controller
     {
-        
-        public IActionResult Index()
+        [Route("Asignatura/Index")]                     //Podemos hacer este enrutamiento que funcionará
+        [Route("Asignatura/Index/{asignaturaId}")]      //para ejecutar nuestas vistas al momento de ejecutarlas en el buscador
+
+        //public IActionResult Index(string id)   Usar el nombre "id" para el parámetro, ya que así lo dicta la convención
+        public IActionResult Index(string asignaturaId) //Se me permite no usar la conveción porque yo defino una nueva arriba con [Route]
         {
-            return View(_context.Asignaturas.FirstOrDefault());  
+            if (!string.IsNullOrWhiteSpace(asignaturaId))
+            {
+                var asignatura = from asig in _context.Asignaturas
+                            where asig.Id == asignaturaId
+                            select asig;
+
+                return View(asignatura.SingleOrDefault());  
+            }
+            else{
+                return View("MultiAsignatura",_context.Asignaturas); 
+            }
         }
+        
         public IActionResult MultiAsignatura()
         {
             return View("MultiAsignatura",_context.Asignaturas); 
