84e883da58763ca33faa6019f54b3a766a08355d
Isaac Puc <dual.isaac.puc@gmail.com>
2022-05-19 10:58:46 -0500

    Refactoring siembra de datos

--- Models/EscuelaContext.cs
+++ Models/EscuelaContext.cs
@@ -18,7 +18,8 @@ namespace ProyectoMVC.Models
 
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder){
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
             base.OnModelCreating(modelBuilder);
 
             var escuela = new Escuela();
@@ -29,41 +30,83 @@ namespace ProyectoMVC.Models
             escuela.Pais = "México";
             escuela.TipoEscuela = TiposEscuela.Secundaria;
             escuela.Dirección = "Boulevard 7ma";
-            
-            modelBuilder.Entity<Escuela>().HasData(escuela); //Sembramos los datos en la base de datos
 
-            modelBuilder.Entity<Asignatura>().HasData( 
-                new Asignatura {
-                Nombre = "Matemáticas",
-                Id = Guid.NewGuid ().ToString ()
-                },
-                new Asignatura {
-                Nombre = "Educación Física",
-                Id = Guid.NewGuid ().ToString ()
-                },
-                new Asignatura {
-                Nombre = "Castellano",
-                Id = Guid.NewGuid ().ToString ()
-                },
-                new Asignatura {
-                Nombre = "Ciencias Naturales",
-                Id = Guid.NewGuid ().ToString ()
-                },
-                new Asignatura {
-                Nombre = "Programacion",
-                Id = Guid.NewGuid ().ToString ()
-                }
-            );
+            //Cargar cursos de la escuela
+            var cursos = CargarCursos(escuela); //Se crearon los cursos pero aun no se mandan al esquema de datos
 
-            modelBuilder.Entity<Alumno>().HasData(GenerarAlumnosAlAzar().ToArray() ); 
-                                                            //HasData SIEMPRE requiere que lo que 
-                                                            //le mande sea un array, por lo tanto 
-                                                            //tenemos que hacerle cast al resultado 
-                                                            //del metodo, ya que este nos devuelve 
-                                                            //una Lista
+            //x cada curso cargar asignaturas
+            var asignaturas = CargarAsignaturas(cursos);
+
+            //x cada curso cargar alumnos
+            var alumnos = CargarAlumnos(cursos);
+
+            //SEMBRAMOS LOS DATOS EN LA BD
+            modelBuilder.Entity<Escuela>().HasData(escuela); 
+            modelBuilder.Entity<Curso>().HasData(cursos.ToArray()); //debido a que le paso una lista y has data funciona con array debo hacer el casteo
+            modelBuilder.Entity<Asignatura>().HasData(asignaturas.ToArray());
+            modelBuilder.Entity<Alumno>().HasData(alumnos.ToArray());
+
         }
 
-        private List<Alumno> GenerarAlumnosAlAzar()
+        private static List<Asignatura> CargarAsignaturas(List<Curso> cursos)
+        {
+            var listaCompleta= new List<Asignatura> ();
+
+            foreach (var curso in cursos)
+            {
+                var tmpList = new List<Asignatura>(){
+                            new Asignatura
+                        {
+                            Nombre = "Matemáticas",
+                            CursoId = curso.Id,
+                            Id = Guid.NewGuid().ToString()
+                        },
+                        new Asignatura
+                        {
+                            Nombre = "Educación Física",
+                            CursoId = curso.Id,
+                            Id = Guid.NewGuid().ToString()
+                        },
+                        new Asignatura
+                        {
+                            Nombre = "Castellano",
+                            CursoId = curso.Id,
+                            Id = Guid.NewGuid().ToString()
+                        },
+                        new Asignatura
+                        {
+                            Nombre = "Ciencias Naturales",
+                            CursoId = curso.Id,
+                            Id = Guid.NewGuid().ToString()
+                        },
+                        new Asignatura
+                        {
+                            Nombre = "Programacion",
+                            CursoId = curso.Id,
+                            Id = Guid.NewGuid().ToString()
+                        }
+                };
+                listaCompleta.AddRange(tmpList);
+            }
+            return listaCompleta;
+        }
+
+        private static List<Curso> CargarCursos(Escuela escuela)
+        {
+            return new List<Curso>(){
+                        new Curso() {
+                                    Id = Guid.NewGuid().ToString(),
+                                    EscuelaId = escuela.Id,
+                                    Nombre = "101",
+                                    Jornada = TiposJornada.Mañana },
+                        new Curso() {Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "201", Jornada = TiposJornada.Mañana},
+                        new Curso   {Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "301", Jornada = TiposJornada.Mañana},
+                        new Curso() {Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "401", Jornada = TiposJornada.Tarde },
+                        new Curso() {Id = Guid.NewGuid().ToString(), EscuelaId = escuela.Id, Nombre = "501", Jornada = TiposJornada.Tarde},
+            };
+        }
+
+        private List<Alumno> GenerarAlumnosAlAzar(Curso curso, int cantidad)
         {
             string[] nombre1 = { "Isaac", "Karol", "Jerry", "Lupita", "Guadalupe", "Nina", "Giselle" };
             string[] apellido1 = { "Puc", "López", "Rojas", "Endes", "Osorno", "Lira", "Herrera" };
@@ -72,10 +115,26 @@ namespace ProyectoMVC.Models
             var listaAlumnos = from n1 in nombre1
                                from n2 in nombre2
                                from a1 in apellido1
-                               select new Alumno { Nombre = $"{n1} {n2} {a1}", Id = Guid.NewGuid().ToString() };
+                               select new Alumno { Nombre = $"{n1} {n2} {a1}", 
+                                                   CursoId = curso.Id,
+                                                   Id = Guid.NewGuid().ToString() 
+                                                  };
 
-            return listaAlumnos.OrderBy((al) => al.Id).ToList();
+            return listaAlumnos.OrderBy((al) => al.Id).Take(cantidad).ToList();
         }
 
+        private List<Alumno> CargarAlumnos(List<Curso> cursos)
+        {
+            var listaAlumnos = new List<Alumno>();
+
+            Random rnd = new Random();
+            foreach (var curso in cursos)
+            {
+                int cantRandom = rnd.Next(5, 20);
+                var tmplist = GenerarAlumnosAlAzar(curso, cantRandom);
+                listaAlumnos.AddRange(tmplist);
+            }
+            return listaAlumnos;
+        }
     }
 }
