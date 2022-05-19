c7c5c90f9802ba54e9ecd1071cc39367a51ba9c0
Isaac Puc <dual.isaac.puc@gmail.com>
2022-05-19 09:16:59 -0500

    Implementando cambios en el modelo

--- Models/Curso.cs
+++ Models/Curso.cs
@@ -4,14 +4,16 @@ using System.Collections.Generic;
 
 namespace ProyectoMVC.Models
 {
-    public class Curso:ObjetoEscuelaBase
+    public class Curso : ObjetoEscuelaBase
     {
         public TiposJornada Jornada { get; set; }
-        public List<Asignatura> Asignaturas{ get; set; }
-        public List<Alumno> Alumnos{ get; set; }
-
+        public List<Asignatura> Asignaturas { get; set; }
+        public List<Alumno> Alumnos { get; set; }
         public string Dirección { get; set; }
 
-
+        public string EscuelaId { get; set; }   //El motor autamaticamente infiere que EscuelaId 
+                                                //proviene del modelo Escuela y de su atributo Id 
+                                                //ya que es un nombre por convención
+        public Escuela Escuela { get; set; }
     }
 }
